08ff84ec5555f5ce3a209c2a14dd8e447707a0ae
Eiji <64912886+Eiji127@users.noreply.github.com>
2020-11-10 23:40:18 +0900

    [追加]: Notification機能追加のためのファイル編集

--- TwitterTutorial/Controller/NotificationController.swift
+++ TwitterTutorial/Controller/NotificationController.swift
@@ -1,18 +1,50 @@
 import UIKit
 
-class  NotificationCotroller: UIViewController {
+private let reuseIdentifier = "NotificationCell"
+
+class  NotificationCotroller: UITableViewController {
+    // MARK: - Properties
+    private var notifications = [Notification]() {
+        didSet{
+            tableView.reloadData()
+        }
+    }
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        fetchNotifications()
     }
     
+    // MARK: - API
+    func fetchNotifications() {
+        NotificationService.shared.fetchNotifications { notifications in
+            self.notifications = notifications
+        }
+    }
+    
+    // MARK: - Helpers
     func configureUI(){
         
         view.backgroundColor = .white
         navigationItem.title = "Notifications"
         
-        
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension NotificationCotroller {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notifications.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        return cell
     }
 }
