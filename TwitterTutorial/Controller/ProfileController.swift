42583b7d14ef1c8c46dbece8ffdd536a3bb95c50
Eiji <64912886+Eiji127@users.noreply.github.com>
2020-11-14 16:08:25 +0900

    [UPDATE]: Fetching & Displaying Liked Tweets

--- TwitterTutorial/Controller/ProfileController.swift
+++ TwitterTutorial/Controller/ProfileController.swift
@@ -49,6 +49,7 @@ class ProfileController: UICollectionViewController {
         super.viewDidLoad()
         configureCollectionView()
         fetchTweets()
+        fetchLikedTweets()
         checkIfUserIsFollowed()
         fetchUserStats()
     }
@@ -65,6 +66,12 @@ class ProfileController: UICollectionViewController {
         TweetService.shared.fetchTweets(forUser: user) { tweets in
             self.tweets = tweets
             self.collectionView.reloadData()
+        }
+    }
+    
+    func fetchLikedTweets() {
+        TweetService.shared.fetchLikes(forUser: user) { tweets in
+            self.likedTweets = tweets
         }
     }
     
@@ -91,6 +98,9 @@ class ProfileController: UICollectionViewController {
         
         collectionView.backgroundColor = .white
         collectionView.contentInsetAdjustmentBehavior = .never
+        
+        guard let tabHeight = tabBarController?.tabBar.frame.height else { return }
+        collectionView.contentInset.bottom = tabHeight
     }
 }
     
@@ -124,7 +134,9 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 120)
+        let viewModel = TweetViewModel(tweet: tweets[indexPath.row])
+        let height = viewModel.size(forWidth: view.frame.width).height
+        return CGSize(width: view.frame.width, height: height + 72)
     }
 }
 
