a6161f0057f8e604fa0575415f16e2e986a3ece2
Eiji <64912886+Eiji127@users.noreply.github.com>
2020-11-14 18:05:32 +0900

    [UPDATE]: Setting Up Reply Label

--- TwitterTutorial/ ViewModel/TweetViewModel.swift
+++ TwitterTutorial/ ViewModel/TweetViewModel.swift
@@ -9,6 +9,8 @@ import UIKit
 
 struct TweetViewModel {
     
+    // MARK: - Properties
+    
     let tweet: Tweet
     let user: User
     
@@ -64,6 +66,17 @@ struct TweetViewModel {
         return UIImage(named: imageName)!
     }
     
+    var shouldHideReplyLabel: Bool {
+        return !tweet.isReply
+    }
+    
+    var replyText: String? {
+        guard let replyingToUsername = tweet.replyingTo else { return nil }
+        return "→ replying to @\(replyingToUsername)"
+    }
+    
+    // MARK: - Lifecycle
+    
     init(tweet: Tweet) {
         self.tweet = tweet
         self.user = tweet.user
@@ -76,6 +89,8 @@ struct TweetViewModel {
         
         return attributedText
     }
+    
+    // MARK: - Helpers
     
     func size(forWidth width: CGFloat) -> CGSize {
         let measurementLabel = UILabel()
