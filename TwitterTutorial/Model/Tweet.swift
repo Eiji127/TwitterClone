e7422e3b67923166c8153053b82619698584f3ed
Eiji <64912886+Eiji127@users.noreply.github.com>
2020-11-14 17:31:23 +0900

    [UPDATE]: Adding Info To Replies In Firebase and Data Model

--- TwitterTutorial/Model/Tweet.swift
+++ TwitterTutorial/Model/Tweet.swift
@@ -16,16 +16,22 @@ struct Tweet {
     let retweetCount: Int
     var user: User
     var didLike = false
+    var replyingTo: String?
     
     init(user: User, tweetID: String, dictonary: [String: Any]) {
         self.tweetID = tweetID
         self.user = user
+        
         self.caption = dictonary["caption"] as? String ?? ""
         self.likes = dictonary["likes"] as? Int ?? 0
         self.retweetCount = dictonary["retweets"] as? Int ?? 0
         
         if let timestamp = dictonary["timestamp"] as? Double {
             self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        if let replyingTo = dictonary["replyingTo"] as? String {
+            self.replyingTo = replyingTo
         }
     }
 }
