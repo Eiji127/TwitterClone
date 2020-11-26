4aeb5fd7f16745d2b9e1902fd82c96d35dea4a60
Eiji <64912886+Eiji127@users.noreply.github.com>
2020-11-26 16:54:17 +0900

    [UPDATE]: Action Button Configuration..

--- TwitterTutorial/Controller/MainTabController.swift
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -8,19 +8,24 @@
 import UIKit
 import Firebase
 
+enum ActionButtonConfiguration {
+    case tweet
+    case message
+}
+
 class MainTabController: UITabBarController {
     
     //MARK: - Properties
     
+    private var buttonConfig: ActionButtonConfiguration = .tweet
+    
     var user: User? {
-        
         didSet{  
             guard let nav = viewControllers?[0] as? UINavigationController else { return }
             guard let feed = nav.viewControllers.first as? FeedCotroller else { return }
             
             feed.user = user
         }
-        
     }
     
     let actionButton: UIButton = {
@@ -34,8 +39,16 @@ class MainTabController: UITabBarController {
     
     //MARK: -Selectors
     @objc func actionButtonTapped() {
-        guard let user = user else { return }
-        let controller = UploadTweetController(user: user, config: .tweet)
+        
+        let controller: UIViewController
+        
+        switch buttonConfig {
+        case .message:
+            controller = SearchController(config: .messages)
+        case .tweet:
+            guard let user = user else { return }
+            controller = UploadTweetController(user: user, config: .tweet)
+        }
         let nav = UINavigationController(rootViewController: controller)
         nav.modalPresentationStyle = .fullScreen
         present(nav, animated: true, completion: nil)
@@ -77,6 +90,8 @@ class MainTabController: UITabBarController {
     // MARK: - Helpers
     
     func configureUI() {
+        self.delegate = self
+        
         view.addSubview(actionButton)
         actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
         actionButton.layer.cornerRadius = 56 / 2
@@ -86,7 +101,7 @@ class MainTabController: UITabBarController {
         let feed = FeedCotroller(collectionViewLayout: UICollectionViewFlowLayout())
         let nav1 = templateNavigationController(image: UIImage(named: "home_unselected")!, rootViewController: feed)
         
-        let explore = ExploreCotroller()
+        let explore = SearchController(config: .userSearch)
         let nav2 = templateNavigationController(image: UIImage(named: "search_unselected")!, rootViewController: explore)
         
         let notifications = NotificationCotroller()
@@ -105,16 +120,14 @@ class MainTabController: UITabBarController {
         
         return nav
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MainTabController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        let index = viewControllers?.firstIndex(of: viewController)
+        let image = index == 3 ? #imageLiteral(resourceName: "mail") : #imageLiteral(resourceName: "new_tweet")
+        self.actionButton.setImage(image, for: .normal)
+        buttonConfig = index == 3 ? .message : .tweet
     }
-    */
-
 }
